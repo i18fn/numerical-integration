02e616c0c7f05d3c19f25d1095531e8e33a88307
i18furuta <fn.nk.2002@gmail.com>
2021-03-14 22:15:58 +0900

    コメントの修正

--- program.go
+++ program.go
@@ -7,13 +7,13 @@ import (
 	"github.com/aclements/go-moremath/stats"
 )
 
+// 台形の面積を求める関数
 func trapezoid(a, b, h float64) float64 {
 	return ((a + b) * h) / 2
 }
 
 const SECTION int = 65536 // 分割数
 
-// 数値積分の高速化
 func integrate(f func(float64) float64, a, b float64) float64 {
 	// 積分区間の分割
 	if SECTION <= 1 {
@@ -48,6 +48,7 @@ func integrate(f func(float64) float64, a, b float64) float64 {
 	return sum
 }
 
+// 標準正規分布表を描いてみるテスト
 func test() {
 	norm := stats.NormalDist{0, 1} // 平均0, 分散1の正規分布
 	z1 := []float64{0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0}
